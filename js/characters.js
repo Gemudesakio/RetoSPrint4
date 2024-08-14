a27e227a632d62fcd6286d07852cdcb72a4c4ded
Luis Rivera <lriveram@unicauca.edu.co>
2024-08-13 19:33:42 -0500

    trabajo del compañero que no aparecio

--- js/characters.js
+++ js/characters.js
@@ -1,1 +1,40 @@
+let urlApi = "https://rickandmortyapi.com/api/character";
 
+const { createApp } = Vue;
+const appCharacter = createApp({
+  data() {
+    return {
+      character: [],
+      characterBk: [],
+      info: {}, // Para manejar la información de paginación
+    };
+  },
+  created() {
+    this.traerData(urlApi);
+  },
+  methods: {
+    traerData(url) {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          this.character = data.results;
+          this.characterBk = data.results;
+          this.info = data.info; // Guardamos la información de paginación
+          console.log(this.characterBk);
+        })
+        .catch((error) => console.error(error));
+    },
+    prevPage() {
+      if (this.info.prev) {
+        this.traerData(this.info.prev);
+      }
+    },
+    nextPage() {
+      if (this.info.next) {
+        this.traerData(this.info.next);
+      }
+    },
+  },
+  computed: {},
+}).mount("#appCharacters");
